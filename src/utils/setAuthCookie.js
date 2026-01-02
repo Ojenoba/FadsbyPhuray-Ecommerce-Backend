22a5494af7b3d9788731854584965340928a47c9
Ojenoba <ojenoba@hotmail.com>
2026-01-02 10:00:23 -0600

    Latest fixes

--- src/utils/setAuthCookie.js
+++ src/utils/setAuthCookie.js
@@ -1,15 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 export const setAuthCookie = (res, token) => {
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: isProd,
-    sameSite: "none",
-    path: "/",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
-
-  // Append Partitioned attribute
   res.append(
     "Set-Cookie",
     `token=${token}; Path=/; HttpOnly; Secure; SameSite=None; Partitioned; Max-Age=${7 * 24 * 60 * 60}`
@@ -17,13 +8,6 @@ export const setAuthCookie = (res, token) => {
 };
 
 export const clearAuthCookie = (res) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: isProd,
-    sameSite: "none",
-    path: "/",
-  });
-
   res.append(
     "Set-Cookie",
     "token=; Path=/; HttpOnly; Secure; SameSite=None; Partitioned; Max-Age=0"
