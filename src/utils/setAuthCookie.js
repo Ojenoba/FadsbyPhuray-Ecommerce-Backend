60aff2421f6ad22826a58858fd32fa508015cee5
Ojenoba <ojenoba@hotmail.com>
2026-01-03 15:00:57 -0600

    fix

--- src/utils/setAuthCookie.js
+++ src/utils/setAuthCookie.js
@@ -3,7 +3,6 @@ export const setAuthCookie = (res, token) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
-    partitioned: true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
@@ -13,6 +12,5 @@ export const clearAuthCookie = (res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
-    partitioned: true,
   });
 };
